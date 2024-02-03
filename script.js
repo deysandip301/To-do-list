2f32f901b742ba0d58667ff7facc1a40190c9484
deysandip301 <deysandip301@gmail.com>
2024-02-03 12:02:27 +0530

    implemented local storage

--- script.js
+++ script.js
@@ -12,6 +12,16 @@ let removeBtn = document.querySelector('.remove-btn');
 let removeTasks = false;
 let colorIndex = 0;
 let colorMap = { 'light-red': 0, 'light-green': 1, 'sky-blue': 2, 'grey-black': 3};
+let ticketArr = [];
+
+
+if(localStorage.getItem('tasks')){
+    let stringifiedArr = localStorage.getItem('tasks');
+    ticketArr = JSON.parse(stringifiedArr);
+    for(let i=0; i<ticketArr.length; i++){
+        generateTask(ticketArr[i].task,ticketArr[i].color,ticketArr[i].id);
+    }
+}
 
 
 addBtn.addEventListener('click', function () {
@@ -31,7 +41,9 @@ addTask.addEventListener('keydown', function (e) {
             alert('Please enter a task');
             return;
         }
-        generateTask(e.target.value);
+
+        generateTask(e.target.value,taskColor);
+        console.log(e.target.value);
         addTask.value = "";
         modal.style.display = 'none';
         addModal = true;
@@ -52,15 +64,28 @@ prioritySelect.addEventListener('click', function (e) {
 var uid = new ShortUniqueId();
 
 
-function generateTask(task,priorityColor) {
-    let id = uid.rnd();
+function generateTask(task,priorityColor,ticketId) {
+
+    let id;
+    if(ticketId){
+        id = ticketId; // it means it is called from local storage ans id is available
+    }
+    else{
+        id = uid.rnd(); // it means it is called from add task button and id is not available
+    }
     let ticketCont = document.createElement('div');
     ticketCont.className = 'ticket-cont';
-    ticketCont.innerHTML = `<div class="ticket-color ${taskColor}">
+    ticketCont.innerHTML = `<div class="ticket-color ${priorityColor}">
                             </div><div class="ticket-id">${id}</div>
                             <div class="ticket-area">${task}</div>
                             <div class="lock"><i class="fa-solid fa-lock"></i></div>`;
     mainTicketCont.appendChild(ticketCont);
+    
+    if(!ticketId) {
+        ticketArr.push({task: task, color: priorityColor, id: id});
+        let stringifiedArr = JSON.stringify(ticketArr);
+        localStorage.setItem('tasks', stringifiedArr);
+    }
 }
 
 removeBtn.addEventListener('click', function () {
@@ -78,6 +103,14 @@ function removeTicket(e) {
     if (e.target.parentNode.classList.contains('ticket-cont')) {
         e.target.parentNode.remove();
     }
+    let id = e.target.parentNode.children[1].innerText;
+    for(let i=0; i<ticketArr.length; i++){
+        if(ticketArr[i].id == id){
+            ticketArr.splice(i,1);
+            break;
+        }
+    }
+    updateLocalStorage();
 }
 
 toolBoxCont.addEventListener('click', function (e) {
@@ -141,3 +174,8 @@ mainTicketCont.addEventListener('click', function (e) {
         }
     }
 });
+
+function updateLocalStorage(){
+    let stringifiedArr = JSON.stringify(ticketArr);
+    localStorage.setItem('tasks',stringifiedArr);
+}
